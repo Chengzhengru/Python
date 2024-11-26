b3ba62dc9fbba13431915cdbae24421b62699f03
Chengzhengru <chengzhengru@gmail.com>
2024-11-27 01:20:05 +0900

    Delete Python_Studies directory

--- Python_Studies/city_functions.py
+++
@@ -1,4 +0,0 @@
-# city_functions.py
-def city_country(city, country):
-
-    return f"{city.title()}, {country.title()}"
--- Python_Studies/test_city_country.py
+++
@@ -1,6 +0,0 @@
-# test_city_country.py
-from city_functions import city_country
-
-def test_city_country():
-    formatted_name = city_country('santiago', 'chile')
-    assert formatted_name == 'Santiago, Chile'
